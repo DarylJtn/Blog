cb801d67d4eef5e3f816fdc1c3cfe1a8c5f8d56b
ThinkPad <daryljtn>
2020-05-04 23:22:04 +0100

    Made baseline changes to title and header

---
+++ content/blog/r-blog-1/index.md
@@ -0,0 +1,5 @@
+---
+title: Performance reporting in R:1
+date: "2020-05-04"
+description: "Getting started in manipulating data to use in R"
+---
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Daryl's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Daryl Johnston`,
+      summary: ``,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
